2e81b08884c10f8c403aeb462050d9d53ade3a5b
Aleksandra Czajczyńska <czajczynska.aleksandra@gmail.com>
2019-12-03 12:50:33 +0100

    release scripts

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React, { PureComponent } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import Config from 'react-native-config';
 
 class App extends PureComponent {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.title}>Espeo Create React Native App</Text>
+        <Text style={styles.title}>{Config.TYPE}</Text>
       </View>
     );
   }
