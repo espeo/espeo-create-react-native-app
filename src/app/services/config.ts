a25d03f654678c6db5cae2d861d04f24c98d5ea0
Aleksandra Czajczyńska <czajczynska.aleksandra@gmail.com>
2019-12-17 16:26:02 +0100

    async-storage patch

---
+++ src/app/services/config.ts
@@ -0,0 +1,65 @@
+import axios from 'axios';
+import { ErrorCodes } from '../constants/constants';
+import asyncStorageService from './async-storage/index';
+
+const baseURL = process.env.API_URL;
+class ApiService {
+  public request: any;
+
+  constructor() {
+    const config = {
+      baseURL,
+      headers: {
+        'Content-Type': 'application/json',
+        ...this.getHeaders(),
+      },
+    };
+
+    this.request = axios.create(config);
+
+    this.request.interceptors.response.use(
+      (response: any) => response,
+      (error: any) => {
+        if (error && error.response) {
+          const { status } = error.response;
+          if (
+            status === ErrorCodes.UNAUTHORIZED ||
+            status === ErrorCodes.ACCESS_DENIED ||
+            status === ErrorCodes.NOT_FOUND
+          ) {
+            // for example logout action here
+          }
+        }
+        return Promise.reject(error);
+      },
+    );
+  }
+
+  public get<T>(url: string, params?: any, options?: {}): Promise<T> {
+    const config = { params, ...options };
+
+    return this.request.get(url, config);
+  }
+
+  public post<T>(url: string, data?: {}, options?: {}): Promise<T> {
+    return this.request.post(url, data, options);
+  }
+
+  private getHeaders() {
+    const token = asyncStorageService.get('token');
+
+    let headers = {};
+
+    if (token) {
+      headers = {
+        'x-auth-token': token,
+      };
+    }
+
+    return {
+      ...headers,
+    };
+  }
+}
+
+export default new ApiService();
