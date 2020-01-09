c3248f3e626aec90f699b47edad641d042fd46e4
Aleksandra Czajczyńska <czajczynska.aleksandra@gmail.com>
2020-01-09 14:32:51 +0100

    patch fix

--- src/app/store/RootReducer.ts
+++ src/app/store/RootReducer.ts
@@ -1,6 +1,6 @@
 import { combineReducers, Reducer } from 'redux';
 
-import { mainScreenReducer } from '@modules/MainScreen/store/reducers';
+import { mainScreenReducer } from '@modules/MainScreen/store/reducers/index';
 
 const rootReducer: Reducer = combineReducers({
   mainScreenReducer,
